bf932e03b738c0c2cfd2f7914162d97c039c0c15
sefercan07 <124098212+sefercan07@users.noreply.github.com>
2023-02-02 17:20:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("hello dunya");
         System.out.println("git inint için");
+        
+        //bakalım bu pull işlemi sadece işlem yaptıgımız proje içinde mi çekiliyor ?
     }
 }
