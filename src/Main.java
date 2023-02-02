ba26ca1119e7ce46c68ba8dcc8cbd68e8e58d15b
Sefer Can <sefercan07@gmail.com>
2023-02-02 17:16:15 +0300

    V5

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("hello dunya");
+        System.out.println("git inint için");
+    }
+}
